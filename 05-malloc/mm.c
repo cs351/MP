16da416ce0b5e3f6d261b5dca813cd3a8d45b966
Michael Lee <lee@soi2.org>
2020-06-27 14:24:12 -0500

    Cleaning up naive allocator

--- 05-malloc/mm.c
+++ 05-malloc/mm.c
@@ -1,58 +1,39 @@
-/*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
-
 #include "mm.h"
 #include "memlib.h"
 
-/* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
- * mm_init - initialize the malloc package.
- */
+
 int mm_init(void)
 {
   return 0;
 }
 
-/* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
 void *mm_malloc(size_t size)
 {
-  int newsize = ALIGN(size + SIZE_T_SIZE);
-  void *p = mem_sbrk(newsize);
-  if ((long)p == -1)
+  int blk_size = ALIGN(size + SIZE_T_SIZE);
+  size_t *header = mem_sbrk(blk_size);
+  if (header == (void *)-1) {
     return NULL;
-  else {
-    *(size_t *)p = size;
-    return (void *)((char *)p + SIZE_T_SIZE);
+  } else {
+    *header = blk_size | 1;
+    return (char *)header + SIZE_T_SIZE;
   }
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
 void mm_free(void *ptr)
 {
+  size_t *header = ptr - SIZE_T_SIZE;
+  *header &= ~1L;
 }
 
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
 void *mm_realloc(void *ptr, size_t size)
 {
   void *oldptr = ptr;
